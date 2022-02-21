73a8c10206b126bda47ccd0af8d1b5eea15d7ca6
Ashwani Kumar <akemma1541990@gmail.com>
2022-02-21 12:10:51 +0530

    serialcallback

--- 11 Promises/callback/parallelFileReading.js
+++ 11 Promises/callback/parallelFileReading.js
@@ -1,14 +1,5 @@
-// Synchronous way of reading data from file
-
-console.log("Before")
 const fs = require('fs')
-let data = fs.readFileSync('f1.txt')
-console.log('Data -> ' + data)
-console.log("After")
 
-
-// Async way of reading file
-//! parallel reading
 console.log("Before")
 fs.readFile('f1.txt',cb)
 function cb(err, data){
